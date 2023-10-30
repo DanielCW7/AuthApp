813a8af1e3c5966393392341c38975937ceb7ee6
daniel <dev.danielcw@gmail.com>
2023-10-29 22:54:00 -0500

    in the process of setting up Auth0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +9,18 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Auth0Provider
+      domain="dev-vgmdplt7mssxdb2p.us.auth0.com"
+      clientId='ivNJoLMCj7rh0uw5OlDlwNxOvCpeyoQS'
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>      
+    </Auth0Provider>
+
   </React.StrictMode>
 );
 
--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,11 +1,14 @@
-
+import LoginButton from "../components/login"
 
 const LoginPage = () => {
+
+
     return (
         <div>
             {/* login page */}
             <h1> Login </h1>
             <p> use <strong> Auth0 </strong> login on this page </p>
+            <LoginButton />
         </div>
     )
 }
